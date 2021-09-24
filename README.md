6a3fff412b030f14630cf915a751e485d3bacdf7
QQKdeGit <627948700@qq.com>
2021-09-24 22:46:06 +0800

    add an optional argument GROUP_NUMBER

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# output-compare
+# 代码对拍器
+
+## 使用说明
+
+首先把两份代码的`.cpp`文件以及能随机生成输入的`.cpp`文件放在根目录下
+
+根据系统类型选择对应的操作
+
+- Windows
+
+  在根目录下打开 `Git Bash` ，输入指令
+
+- Linux & macOS
+
+  直接在终端里运行指令
+
+指令如下
+
+  ```shell
+  bash comparator.sh input.cpp prog1.cpp prog2.cpp
+  ```
+
+> `input.cpp` 替换为能随机生成输入的文件名
+>
+> `prog1.cpp` 替换为第一份代码的文件名
+>
+> `prog2.cpp` 替换为第二份代码的文件名
+
+
+
+## 后续开发
+
+- 开发GUI版本
--- comparator.sh
+++ comparator.sh
@@ -1,9 +1,10 @@
 #!/bin/env bash
 
-# bash comparator.sh <input.cpp> <prog1.cpp> <prog2.cpp>
+# bash comparator.sh <input.cpp> <prog1.cpp> <prog2.cpp> <groupNumber>
 INPUT_SOURCE=$1
 PROG1_SOURCE=$2
 PROG2_SOURCE=$3
+GROUP_NUMBER=$4
 
 # Colors
 BOLD="\033[1m"
@@ -19,6 +20,7 @@ HEADER_INFO="${BLUE}[INFO]$OFF "
 HEADER_WARN="${YELLOW}[WARN]$OFF "
 HEADER_ERROR="${RED}[ERROR]$OFF "
 HEADER_BUILD="${PURPLE}[BUILD]$OFF "
+HEADER_SUCCESS="${GREEN}[SUCCESS]$OFF "
 
 CC="gcc"
 CFLAGS="-std=c11 -g"
@@ -36,6 +38,18 @@ compileSource() {
         echo -e "${GREEN} done${OFF}"
     else
         exit 1
+    fi
+}
+
+deleteEXE() {
+    if [[ $(uname) =~ NT ]]; then
+        rm -f ${INPUT_SOURCE//.cpp/.exe}
+        rm -f ${PROG1_SOURCE//.cpp/.exe}
+        rm -f ${PROG2_SOURCE//.cpp/.exe}
+    else
+        rm -f ${INPUT_SOURCE//.cpp/}
+        rm -f ${PROG1_SOURCE//.cpp/}
+        rm -f ${PROG2_SOURCE//.cpp/}
     fi
 }
 
@@ -59,8 +73,17 @@ while [[ $# -gt 0 ]]; do
         echo -e "${HEADER_ERROR}${YELLOW}INPUT: ${OFF}\n${INPUT}\n"
         echo -e "${HEADER_ERROR}${BOLD}${PROG1_SOURCE//.cpp/} ${YELLOW}OUTPUT: ${OFF}\n${PROG1}\n"
         echo -e "${HEADER_ERROR}${BOLD}${PROG2_SOURCE//.cpp/} ${YELLOW}OUTPUT: ${OFF}\n${PROG2}\n"
+        deleteEXE
         break
     fi
 
     echo -e "${GREEN} done${OFF}"
+
+    if [[ $GROUP_NUMBER ]]; then
+        if [[ COUNTER -eq GROUP_NUMBER ]]; then
+            echo -e "\n${HEADER_SUCCESS}Congratulations! There is no difference!"
+            deleteEXE
+            break
+        fi
+    fi
 done
